07ed0334cdc5716b11eb1c724e21e42cdf66c04e
Samuel <samiequipo@gmail.com>
2020-06-13 19:43:31 -0300

    Adds show action to the users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, uniqueness: true,
                     length: {minimum:3, maximum:25}, format: { with: VALID_EMAIL_REGEX }
-  validates :password_digest, presence: true
   
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,17 +5,24 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(email: "juancho@gmail.com", password: 'test')
+    @user = User.new(email: "juancho@gmail.com", password_digest: 'test')
     @user.save
   end
 
-  test 'user with a valid email should be valid' do
-    user = User.new(email: 'test@test.org', password: 'test')
+  test 'user should have a valid email' do
+    user = User.new(email: 'test@test.org', password_digest: 'test')
     assert user.valid?   
   end
 
   test 'user with invalid email should be invalid' do
-    user = User.new(email: 'test', password: 'test')
+    user = User.new(email: 'test', password_digest: 'test')
+    assert_not user.valid?
+  end
+
+  test 'user with taken email should be invalid' do
+    other_user = users(:one)
+    user = User.new(email: other_user.email, password_digest:
+    'test')
     assert_not user.valid?
   end
 end
